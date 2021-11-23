c71b95e0f67186e064ee8641efb228b69b8f2306
Jacob Swartz <jacob.sf.us@gmail.com>
2021-11-22 19:59:41 -0800

    Allow passing directory arg to script or fallback

--- rarbg-clean.sh
+++ rarbg-clean.sh
@@ -3,7 +3,15 @@
 # Removes junk included in a torrent downloaded from RARBG, including Samples
 # Typical usage is script execution following a completed download
 
-# ! Remember to `cd` into your appropriate directory
+# Target directory is from where the script was run, or a valid directory argument
 
-find . -depth -type f \( -name "RARBG_DO_NOT_MIRROR.exe" -o -name "RARBG.txt" \) -o -type d -name "Sample" | xargs rm -rfv
-find . -type d -empty -delete -print
+TARGET_DIR=$(pwd)
+
+if [ ! -z $1 ]; then
+    if [ -d $1 ]; then
+        TARGET_DIR=$1
+    fi
+fi
+
+find "$TARGET_DIR" -depth -type f \( -name "RARBG_DO_NOT_MIRROR.exe" -o -name "RARBG.txt" \) -o -type d -name "Sample" | xargs rm -rfv
+find "$TARGET_DIR" -type d -empty -delete -print
